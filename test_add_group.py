4a492c760b0aa80b4881f3699a10a03ff15ccf9a
aleks21977 <afonin_av@list.ru>
2018-09-21 16:05:53 +0300

    Убрал лишнее

--- test_add_group.py
+++ test_add_group.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -12,15 +12,17 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
+#        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
+#        wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").click()
+#        wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("asdf")
         wd.find_element_by_name("group_header").click()
