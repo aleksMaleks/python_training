fcaf92f3e94616a3e1c41e78ec89979e648110a8
aleks21977 <afonin_av@list.ru>
2018-09-27 12:22:11 +0300

    Lecture2_2(fixture)

--- test_add_contact.py
+++ test_add_contact.py
@@ -8,12 +8,15 @@ from group import Group
 class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(60)
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -22,7 +25,8 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def create_contact(self, wd, group):
+    def create_contact(self, group):
+        wd = self.wd
         # init contact creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
@@ -37,28 +41,25 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("lastname").send_keys(group.footer)
         # submit contcat creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_home_page()
 
-    def return_to_home_page(self, wd):
+    def return_to_home_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("home page").click()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
     def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, Group(name="first1", header="middle1", footer="last1"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.create_contact(Group(name="first1", header="middle1", footer="last1"))
+        self.logout()
 
     def test_add_empty_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, Group(name="", header="", footer=""))
-        self.return_to_home_page(wd)
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.create_contact(Group(name="", header="", footer=""))
+        self.logout()
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
