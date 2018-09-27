5e78ebb6befede77b8fe0b1b70c0aaf677469ddd
aleks21977 <afonin_av@list.ru>
2018-09-27 13:47:34 +0300

    Выделен вспомогательный класс SessionHelper (для управления сессиями)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,14 +13,14 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Group(name="first1", header="middle1", footer="last1"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 # def is_element_present(self, how, what):
 #     try: self.wd.find_element(by=how, value=what)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -19,15 +19,15 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="asdf", header="asdf", footer="asdfsdf"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
 # def is_element_present(self, how, what):
