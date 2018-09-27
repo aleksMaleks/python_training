30702bf5a65a58b5855d8843b6d3ab8256894300
aleks21977 <afonin_av@list.ru>
2018-09-27 14:04:26 +0300

    Выделены классы для работы с группами и контактами

---
+++ fixture/contact.py
@@ -0,0 +1,29 @@
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create(self, group):
+        wd = self.app.wd
+        # init contact creation
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(group.name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(group.header)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(group.footer)
+        # submit contcat creation
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_home_page()
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
