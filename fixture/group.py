554197c83edcfe5b2c1edbbec43b9bebcbe81999
Home <afonin_av@list.ru>
2018-10-09 17:52:52 +0300

    lesson 3_5

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -84,3 +85,10 @@ class GroupHelper:
     def return_to_home(self):
         wd = self.app.wd
         wd.find_element_by_css_selector("a[href='./']").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
+
