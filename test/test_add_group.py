c9f5fdac1ab8d6972a9040db59b9f27438dc4288
Home <afonin_av@list.ru>
2018-10-11 23:54:23 +0300

    Temp 4_3_sorting_lists

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,12 +11,27 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.group.create(Group(name="asdf", header="asdf", footer="asdfsdf"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="asdf", header="asdf", footer="asdfsdf")
+    app.group.create(group)
 #    app.session.logout() # эмуляция аварийного завершения сессии
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
 
 
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+    old_groups = app.group.get_group_list()
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 
 # def is_element_present(self, how, what):
