670c2ad04da3baa261a478570b573c6b8f644747
Home <afonin_av@list.ru>
2018-10-13 16:10:32 +0300

    Temp5

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -6,15 +6,19 @@ def test_modify_contact_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test7777"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(firstname="new name1"))
+    contact = Contact(firstname="new firstname1")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_modify_contact_header(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="test7777"))
-    old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(middlename="new header1"))
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
+# def test_modify_contact_header(app):
+#     if app.contact.count() == 0:
+#         app.contact.create(Contact(middlename="test7777"))
+#     old_contacts = app.contact.get_contact_list()
+#     app.contact.modify_first_contact(Contact(middlename="new middlename1"))
+#     new_contacts = app.contact.get_contact_list()
+#     assert len(old_contacts) == len(new_contacts)
