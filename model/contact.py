d4d1ddb78c30bb7267d0f5cc4291a411aebe5dec
Home <afonin_av@list.ru>
2018-10-13 15:59:57 +0300

    Temp4

--- model/contact.py
+++ model/contact.py
@@ -14,3 +14,8 @@ class Contact:
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname
 
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from sys import maxsize
 
 from model.contact import Contact
 
@@ -12,19 +11,17 @@ def test_add_contact(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
-    def id_or_max(gr):
-        if gr.id:
-            return int(gr.id)
-        else:
-            return maxsize
-    assert sorted(old_contacts, key=id_or_max) == sorted(new_contacts, key=id_or_max)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-# def test_add_empty_contact(app):
-#     old_contacts = app.contact.get_contact_list()
-#     app.contact.create(Contact(name="", header="", footer=""))
-#     new_contacts = app.contact.get_contact_list()
-#     assert len(old_contacts) + 1 == len(new_contacts)
+def test_add_empty_contact(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="", middlename="", lastname="")
+    app.contact.create(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
