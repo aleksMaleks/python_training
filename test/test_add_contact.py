e87e0144d43a6552d506ba318260a08280823bf5
HomeName <afonin_av@list.ru>
2018-10-26 15:24:27 +0300

    DZ15

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,30 +1,27 @@
 # -*- coding: utf-8 -*-
-
+import random
+import string
+import pytest
 from model.contact import Contact
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " # из за пробелов иногда тест падает!
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-def test_add_contact(app):
+testdata = [Contact(firstname="", middlename="", lastname="")] + [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 20), lastname=random_string("lastname", 20))
+    for i in range(2)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="firstname1", middlename="middlename1", lastname="lastname1")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-# def test_add_empty_contact(app):
-#     old_contacts = app.contact.get_contact_list()
-#     contact = Contact(firstname="", middlename="", lastname="")
-#     app.contact.create(contact)
-#     new_contacts = app.contact.get_contact_list()
-#     assert len(old_contacts) + 1 == len(new_contacts)
-#     old_contacts.append(contact)
-#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-
-
 
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import pytest
 from model.group import Group
+import random
+import string
+
 
 # def is_alert_present(wd): # пример взят из урока
 #     try:
@@ -9,28 +12,36 @@ from model.group import Group
 #     except:
 #         return False
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " # *10 убрал генерацию лишних пробелов так как с ними некоторые тесты падают, а еще не создается группа со знаком ' (string.punctuation + )
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="asdf", header="asdf", footer="asdfsdf")
-    app.group.create(group)
+# первый вариант
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(2)
+]
+
+# второй вариант
+# testdata = [
+#     Group(name=name, header=header, footer=footer)
+#     for name in ["", random_string("name", 10)]
+#     for header in ["", random_string("header", 20)]
+#     for footer in ["", random_string("footer", 20)]
+# ]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
+#    pass
+     old_groups = app.group.get_group_list()
+     app.group.create(group)
 #    app.session.logout() # эмуляция аварийного завершения сессии
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+     assert len(old_groups) + 1 == app.group.count()
+     new_groups = app.group.get_group_list()
+     old_groups.append(group)
+     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-
-
-
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
