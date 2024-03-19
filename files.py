0564462c488b00c349e7961f37849bac4db71702
aleksMaleks <35885304+aleksMaleks@users.noreply.github.com>
2024-03-19 17:03:17 +0300

    Update files.py

--- files.py
+++ files.py
@@ -6,7 +6,7 @@ with open("C:/tools/config.json") as f: # см урок 27_with.mp4
     except ValueError as ex: # перехват исключения ValueError
         print(ex)
         res = {}
-# finally: # блок finally выполняется при любых обстоятельствах см урок 26_exceptions.mp4
+# finally: # блок finally выполняется при любых обстоятельствах
 #     f.close()
 
 print(res)
